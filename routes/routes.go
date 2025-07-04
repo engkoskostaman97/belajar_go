0edbba9aa5d80b28427432629d9bc451ca8b1a31
engkos kostaman <engkos.vendor.mandati@pegadaian.co.id>
2025-07-04 11:56:47 +0700

    crud go

--- routes/routes.go
+++ routes/routes.go
@@ -4,6 +4,7 @@ import (
 	"santrikoding/backend-api/controllers"
 	"santrikoding/backend-api/middlewares"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +12,14 @@ func SetupRouter() *gin.Engine {
 
 	//initialize gin
 	router := gin.Default()
+
+	// set up CORS
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:  []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders: []string{"Content-Length"},
+	}))
 
 	// route register
 	router.POST("/api/register", controllers.Register)
@@ -26,6 +35,12 @@ func SetupRouter() *gin.Engine {
 
 	// route user by id
 	router.GET("/api/users/:id", middlewares.AuthMiddleware(), controllers.FindUserById)
+
+	// route user update
+	router.PUT("/api/users/:id", middlewares.AuthMiddleware(), controllers.UpdateUser)
+
+	// route user delete
+	router.DELETE("/api/users/:id", middlewares.AuthMiddleware(), controllers.DeleteUser)
 
 	return router
 }
