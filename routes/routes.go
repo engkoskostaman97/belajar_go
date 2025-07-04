b823af7d9663ff541d771f0886be46de6f0d38d3
engkos kostaman <engkos.vendor.mandati@pegadaian.co.id>
2025-07-04 11:49:09 +0700

    crud go

---
+++ routes/routes.go
@@ -0,0 +1,31 @@
+package routes
+
+import (
+	"santrikoding/backend-api/controllers"
+	"santrikoding/backend-api/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+
+	//initialize gin
+	router := gin.Default()
+
+	// route register
+	router.POST("/api/register", controllers.Register)
+
+	// route login
+	router.POST("/api/login", controllers.Login)
+
+	// route users
+	router.GET("/api/users", middlewares.AuthMiddleware(), controllers.FindUsers)
+
+	// route user create
+	router.POST("/api/users", middlewares.AuthMiddleware(), controllers.CreateUser)
+
+	// route user by id
+	router.GET("/api/users/:id", middlewares.AuthMiddleware(), controllers.FindUserById)
+
+	return router
+}
